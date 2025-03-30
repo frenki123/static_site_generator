cac12371924f4347303e90819e0bc9cde522ca09
frenki <frenki@email.com>
2025-03-30 22:41:25 +0200

    Feat:Text nodes

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from textnode import TextNode, TextType
+#TextNode(This is some anchor text, link, https://www.boot.dev)
+print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
