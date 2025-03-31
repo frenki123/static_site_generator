04460a40688811ced9917b9c479329df4a54fefc
frenki <frenki@email.com>
2025-03-31 18:42:21 +0200

    Feat: Conversion from TextNode to LeafNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -19,5 +19,51 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.CODE)
         self.assertNotEqual(node, node2)
 
+class TestTextNodeConversion(unittest.TestCase):
+    def test_text(self):
+        txt = TextNode("some text", TextType.TEXT)
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, None)
+        self.assertEqual(n.value, "some text")
+
+    def test_bold(self):
+        txt = TextNode("some text", TextType.BOLD)
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "b")
+        self.assertEqual(n.value, "some text")
+
+    def test_italic(self):
+        txt = TextNode("some text", TextType.ITALIC)
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "i")
+        self.assertEqual(n.value, "some text")
+
+    def test_code(self):
+        txt = TextNode("some text", TextType.CODE)
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "code")
+        self.assertEqual(n.value, "some text")
+
+    def test_link(self):
+        txt = TextNode("some text", TextType.LINK, "someurl")
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "a")
+        self.assertEqual(n.value, "some text")
+        self.assertEqual(n.props, {"href":"someurl"})
+
+    def test_link_no_url(self):
+        txt = TextNode("some text", TextType.LINK)
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "a")
+        self.assertEqual(n.value, "some text")
+        self.assertEqual(n.props, {"href":"#"})
+
+    def test_img(self):
+        txt = TextNode("image", TextType.IMAGE, "someurl")
+        n = txt.to_html_node()
+        self.assertEqual(n.tag, "img")
+        self.assertEqual(n.value, "")
+        self.assertEqual(n.props, {"alt":"image", "src":"someurl"})
+
 if __name__ == "__main__":
     _ = unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -2,7 +2,10 @@ from __future__ import annotations
 from enum import Enum
 from typing import override
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -26,3 +29,25 @@ class TextNode:
     @override
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    def to_html_node(self) -> LeafNode:
+        match self.text_type:
+            case TextType.TEXT:
+                return LeafNode(None, self.text)
+            case TextType.BOLD:
+                return LeafNode("b", self.text)
+            case TextType.ITALIC:
+                return LeafNode("i", self.text)
+            case TextType.CODE:
+                return LeafNode("code", self.text)
+            case TextType.LINK:
+                url = "#"
+                if self.url is not None:
+                    url = self.url
+                return LeafNode("a", self.text, {"href":url})
+            case TextType.IMAGE:
+                url = "#"
+                if self.url is not None:
+                    url = self.url
+                return LeafNode("img", "", {"src":url,"alt":self.text})
+
