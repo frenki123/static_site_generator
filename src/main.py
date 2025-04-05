78ff60015a5a0fc10ffdb3c906ea6cfb6615a68a
frenki <frenki@email.com>
2025-04-06 00:50:45 +0200

    Feat: copy files

--- src/main.py
+++ src/main.py
@@ -1,21 +1,29 @@
-from parser import *
-from textnode import TextNode, TextType
+from ntpath import isfile
+import os
+import shutil
+
+def copy_files(src:str, des:str):
+    if not os.path.exists(src) or not os.path.exists(des):
+        raise Exception("Not valid filepaths")
+    shutil.rmtree(des)
+    os.mkdir(des)
+    copy_tree(src, des)
+
+def copy_tree(src:str, des:str):
+    if os.path.isfile(src):
+        shutil.copy(src, des)
+        return
+    objects = os.listdir(src)
+    for obj in objects:
+        if os.path.isfile(obj):
+            shutil.copy(obj, des)
+        else:
+            new_des = os.path.join(des, obj)
+            os.mkdir(new_des)
+            new_src = os.path.join(src,obj)
+            copy_tree(new_src, new_des)
+
+if __name__ == "__main__":
+    copy_files("static", "public")
 
 
-def test_split_images():
-    node = TextNode(
-        "This is text with an [image](https://i.imgur.com/zjjcJKZ.png) and another [image](https://i.imgur.com/zjjcJKZ.png))",
-        TextType.TEXT,
-    )
-    new_nodes = split_nodes_by_type([node],extract_markdown_links)
-    print(new_nodes)
-    res = [
-            TextNode("This is text with an ", TextType.TEXT),
-            TextNode("image", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
-            TextNode(" and another ", TextType.TEXT),
-            TextNode("image", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
-            TextNode(")", TextType.TEXT),
-        ]
-    print(res)
-
-test_split_images()
